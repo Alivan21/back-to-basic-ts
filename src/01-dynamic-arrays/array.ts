c16a1873af9e5358e98831201a275c5b3d7940f7
Alivan_FE <alfan.olivan@formulatrix.com>
2026-05-12 19:14:04 +0700

    feat: unfinished pop logic

--- src/01-dynamic-arrays/array.ts
+++ src/01-dynamic-arrays/array.ts
@@ -1,5 +1,5 @@
 class Array<T> {
-  private data: (T | null)[] = [];
+  private data: (T | null | undefined)[] = [];
   private length: number = 0;
 
   constructor(length?: number) {
@@ -20,6 +20,13 @@ class Array<T> {
   }
 
   push(item: T): void {
+    for (let index = 0; index < this.length; index++) {
+      if (this.data[index] == null) {
+        this.data[index] = item;
+        return;
+      }
+    }
+
     this.data[this.length] = item;
     this.length++;
   }
@@ -30,6 +37,42 @@ class Array<T> {
     delete this.data[this.length - 1];
     this.length--;
     return lastItem;
+  }
+
+  shift(): T | null | undefined {
+    if (this.length === 0) return undefined;
+    const firstItem = this.data[0];
+    for (let index = 1; index < this.length; index++) {
+      this.data[index - 1] = this.data[index];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  }
+
+  unshift(item: T): void {
+    for (let index = this.length; index > 0; index--) {
+      this.data[index] = this.data[index - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+  }
+
+  splice(start: number, deleteCount: number, ...items: T[]): void {
+    const removedItems: (T | null | undefined)[] = [];
+    for (let index = start; index < start + deleteCount && index < this.length; index++) {
+      removedItems.push(this.data[index]);
+    }
+    for (let index = start + deleteCount; index < this.length; index++) {
+      this.data[index - deleteCount + items.length] = this.data[index];
+    }
+    for (let index = 0; index < items.length; index++) {
+      this.data[start + index] = items[index];
+    }
+    for (let index = this.length - 1; index >= this.length - deleteCount + items.length; index--) {
+      delete this.data[index];
+    }
+    this.length = this.length - deleteCount + items.length;
   }
 
   toString(): string {
--- src/01-dynamic-arrays/index.ts
+++ src/01-dynamic-arrays/index.ts
@@ -2,6 +2,11 @@ import Array from "./array";
 
 export default function MainArray() {
   const array = new Array<number>(5);
+  array.push(1);
+  array.push(2);
+  array.push(3);
+
+  array.pop();
 
   console.log("Array length:", array.size);
   console.log("Array data:", array.toString());
